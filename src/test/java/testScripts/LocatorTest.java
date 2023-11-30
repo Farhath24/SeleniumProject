ff81adde1e42502d0fad3e778c6466b38cabb8d6
Farhath24 <shaikfarhath888@gmail.com>
2023-11-30 07:56:49 +0000

    "Third commit of the session"

--- src/test/java/testScripts/LocatorTest.java
+++ src/test/java/testScripts/LocatorTest.java
@@ -1,6 +1,10 @@
 package testScripts;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatorTest {
@@ -8,6 +12,11 @@ public class LocatorTest {
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().deleteAllCookies();
+		List<WebElement> elements=driver.findElements(By.xpath("//td[text='B.Wagner']//following-sibling::td"));
+		for(WebElement cellItems:elements)
+		{
+			System.out.println(cellItems.getText());
+		}
 		
 
 	}
